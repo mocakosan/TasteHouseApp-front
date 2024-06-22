3fb99a97f0cb6292c59e760f7a64abcd4490baae
JangsanHighDev <mocakosan96@highdev.pro>
2024-06-22 23:12:03 +0900

    Add : google maps connect

--- ios/TasteHouseApp/AppDelegate.mm
+++ ios/TasteHouseApp/AppDelegate.mm
@@ -1,11 +1,13 @@
 #import "AppDelegate.h"
 
+#import <GoogleMaps/GoogleMaps.h>
 #import <React/RCTBundleURLProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyAFaZPiwCIUfTTHQPH8vwDl0_-c3cVy4iM"];
   self.moduleName = @"TasteHouseApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
--- src/screens/map/MapHomeScreen.tsx
+++ src/screens/map/MapHomeScreen.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 import useAuth from '@/hooks/queries/useAuth';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
 
 function MapHomeScreen() {
   const {logoutMutation} = useAuth();
   return (
-    <View>
-      <Text>맵 스크린</Text>
-      <Button title="로그아웃" onPress={() => logoutMutation.mutate(null)} />
-    </View>
+    <MapView
+      style={styles.container}
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation
+      followsUserLocation
+      showsMyLocationButton={true}
+    />
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default MapHomeScreen;
