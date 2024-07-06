02552b4d30070bb2cd6d0c319ff4f80a9533160a
JangsanHighDev <mocakosan96@highdev.pro>
2024-07-07 00:49:48 +0900

    fix : bottomtab in feedScreen

--- src/screens/feed/FeedFavoriteScreen.tsx
+++ src/screens/feed/FeedFavoriteScreen.tsx
@@ -1,3 +1,4 @@
+import FeedFavoriteList from '@/components/feed/FeedFavoriteList';
 import {colors} from '@/constants';
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
@@ -5,7 +6,7 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 function FeedFavoriteScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <FeedFavoriteScreen />
+      <FeedFavoriteList />
     </SafeAreaView>
   );
 }
